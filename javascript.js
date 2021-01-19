1407817b5f7b19b505498aeff9a9fe3b76140ce3
Toriong <67920345+Toriong@users.noreply.github.com>
2021-01-18 20:53:46 -0600

    functions added
    Added functions to my to-do list.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-var addButton;
-addButton = document.querySelector(".add-button");
 
 var toDoList;
 toDoList = document.querySelector("#confirmed-to-do-list");
@@ -7,63 +5,99 @@ toDoList = document.querySelector("#confirmed-to-do-list");
 var userInput;
 userInput = document.querySelector(".to-do-input");
 
-
-
-
-
 //END GOAL: add to the to-do list
 //steps:
 //create new li
 //append to the new li the user input
 //append that new li to the to do list on the html page
-addButton.addEventListener("click", function () {
+
+var completionList = document.querySelector('#user-completion-list')
+var userDueDateInput = document.querySelector("#due-date");
+var ToDoEntry = document.createTextNode(userInput.value)
+
+
+function newListItem() {
+    var newLi = document.createElement("li")
+    return newLi;
+}
+
+function addToDo() {
+    
     var newLi = document.createElement("li");
-    // var span = document.createElement("span");
-    // var delButtonText = document.createTextNode("DEL");
-    // var createDelButton = document.createdElement("button");
+    var singleSeparator = document.createTextNode(" | ");
+    var dueDateText = document.createTextNode(" | DUE ON: ");
     // this is making a textnode
-    var confirmedToDoEntry = document.createTextNode(userInput.value);
-    newLi.appendChild(confirmedToDoEntry);
-    // createDelButton.appendChild(delButtonText);
-    // span.appendChild(createDelButton);
-    // span.appendChild(newLi)
-
-    
+    var ToDoEntry = document.createTextNode(userInput.value)
+    var dueDate = document.createTextNode(userDueDateInput.value)
+    newLi.appendChild(ToDoEntry)
+    newLi.appendChild(singleSeparator);
+    newLi.appendChild(dueDateText);
+    newLi.appendChild(dueDate);
+    newLi.appendChild(singleSeparator);
+    var completedButton = document.createElement("button");
+    var completedButtonText = document.createTextNode("COMPELETED")
+    completedButton.appendChild(completedButtonText);
+    newLi.appendChild(completedButton);
+    var delButton = document.createElement("button");
+    var delButtonText = document.createTextNode("DEL");
+    delButton.appendChild(delButtonText);
+    newLi.appendChild(delButton);
     toDoList.appendChild(newLi);
+    function delFromToDoListPromptAndRemoval(){
+        if (confirm("Do you want to delete this entry?")) {
+        newLi.remove("li");
+        alert("Entry has been deleted.")
+    } else {
+        alert("No changes has occured.")
+    }
+    }
+    delButton.addEventListener("click", delFromToDoListPromptAndRemoval)
+    function completedButtonPromptAndMoveToComp() {
+        if (confirm("You pressed 'completed.' This is task will now be inserted into your completion list. Press 'Ok' to continue.")) {
+        newLi.removeChild(dueDate);
+            newLi.removeChild(dueDateText);
+            // add attribute: "complete on *current date*"
+        newLi.removeChild(completedButton)
+        completionList.appendChild(newLi);
+            
+        
+        } else {
+            alert("No changes has occured.")
+        }
+}
+    completedButton.addEventListener("click", completedButtonPromptAndMoveToComp);
 
-
-})
-
-// addButton.addEventListener("click", function () {
-//     var newLi = document.createElement("li");
-//     // var span = document.createElement("span");
-//     // var delButtonText = document.createTextNode("DEL");
-//     // var createDelButton = document.createdElement("button");
-//     //this is making a textnode
-//     var confirmedToDoEntry = document.createTextNode(userInput.value);
-//     newLi.appendChild(confirmedToDoEntry);
-//     // createDelButton.appendChild(delButtonText);
-//     // span.appendChild(createDelButton);
-//     // span.appendChild(newLi)
-
+}
     
-//     toDoList.appendChild(newLi);
+
+var submitButton = document.querySelector(".submit");
+submitButton.addEventListener("click", addToDo);
+
+// create an array to store to-do's
+var toDoArray = [];
+//create an object to name to-do's
+
+function toDoName(name) {
+    this.name = name;
+}
+
+function addToDoToArray() {
+    var add = toDoName(userInput.value)
+    toDoArray.push(add);
+    saveToDo();
+}
+//to save to-do's from the user into the local storage
+function saveToDo() {
+    var jsonStrToDo = JSON.stringify(toDoArray);
+    localStorage.setItem("todos",jsonStrToDo);
+}
 
 
-// })
 
-// toDoList.classList.add("listDecoration");
+//create an array to store the due-dates
+// var toDoDueDateArray = [];
 
-// var allEntriesOnToDoList = document.querySelectorAll('#confirmed-to-do-list li');
-// var plusIcon = 
 
-// for (user of allEntriesOnToDoList) {
-    
-// }
-// var delButton = document.querySelector("del-button");
-
-// delButton.addEventListener("click", function () {
-    
-// })
+// var userDueDatesStoredArray = [];
 
 

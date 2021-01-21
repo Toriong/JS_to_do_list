91c46b4dfb028978c868d987a08cbcf3f64143ed
Toriong <67920345+Toriong@users.noreply.github.com>
2021-01-20 21:16:53 -0600

    Added JS arrays for the user To-do entries
    Currently working on having the user entries be saved to the local storage and have it displayed after the refresh of the page.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
 
 var toDoList;
 toDoList = document.querySelector("#confirmed-to-do-list");
@@ -5,11 +9,6 @@ var userInput;
 
 userInput = document.querySelector(".to-do-input");
 
-//END GOAL: add to the to-do list
-//steps:
-//create new li
-//append to the new li the user input
-//append that new li to the to do list on the html page
 
 var completionList = document.querySelector('#user-completion-list')
 var userDueDateInput = document.querySelector("#due-date");
@@ -46,19 +45,23 @@ function completedButtonPromptAndMoveToComp(listItem, userDueDate,textDisplayDue
 var toDoArray = [];
 
 function save() {
-    return localStorage.setItem("Todos", toDoArray);
+    localStorage.setItem("All Entries", JSON.stringify(toDoArray));
 }
 
-var deletedItems = toDoArray.filter(function (value) {
-    return 
-})
-function addUserInputToArray() {
-    storeInArray = toDoArray.push(userInput.value)
-    stringifyToDoArray = JSON.stringify(storeInArray);
-    save()
-    return stringifyToDoArray;
+
+
+
+// function addUserInputToArray() {
+//     userInputValueAsObject = new givveNameToToDo(userInput.value);
+//     storeInArray = toDoArray.push(userInputValueAsObject);
+//     stringifyToDoArray = JSON.stringify((storeInArray));
+//     save()
+//     return stringifyToDoArray;
     
-}
+// }
+
+
+
 function delFromToDoListPromptAndRemoval(listItem) {
     return function () {
         if (confirm("Do you want to delete this entry?")) {
@@ -69,6 +72,10 @@ function delFromToDoListPromptAndRemoval(listItem) {
         }
     }
 }
+
+
+
+
 
 function addToDo() {
     var newLi = document.createElement("li");
@@ -90,23 +97,72 @@ function addToDo() {
     var delButtonText = document.createTextNode("DEL");
     delButton.appendChild(delButtonText);
     newLi.appendChild(delButton);
-    toDoList.appendChild(newLi);
-
-    delButton.addEventListener("click", delFromToDoListPromptAndRemoval(newLi))
+    toDoList.prepend(newLi);
+    delButton.addEventListener("click", delFromToDoListPromptAndRemoval(newLi));
+    
+    // GOAL:
+    delButton.addEventListener("click", deleteItemInToDoArray(document.querySelector("li").firstChild.textContent));
+    delButton.addEventListener("click", deleteItemInToDoArray(userInput.value));
     completedButton.addEventListener("click", completedButtonPromptAndMoveToComp(newLi, dueDate, dueDateText, completedButton));
 
 }
 
+function deleteItemInToDoArray(deleteThisItem) {
+    var filteredToDoArray = toDoArray.filter(function (e) { return e !== deleteThisItem })
+    return function () {
+        localStorage.setItem("Official To-Do List", JSON.stringify(filteredToDoArray));
+        console.log(filteredToDoArray);
+    }
+}
 
-
-
+// function giveNameToToDo(name) {
+//     this.name = name; 
+    
+    
+// }
+function addToDoArray() {
+    var toDoInsertedToArray = toDoArray.push(userInput.value)
+    save(); 
+    return toDoInsertedToArray;
+}
 var submitButton = document.querySelector(".submit");
+
+// function insertClass() {
+//     var selectLi = document.querySelector("li");
+//     return selectLi.classList.add(".container");
+// }
+
+function selectUserInputAsText() {
+    x = document.querySelector("li").firstChild.textContent;
+    return x;
+}
+
+// function storeUserInputIntoVar() {
+//     y = selectText();
+//     return y; 
+// }
+
 submitButton.addEventListener("click", addToDo);
+submitButton.addEventListener("click", addToDoArray);
+// submitButton.addEventListener("click", insertClass);
+submitButton.addEventListener("click", selectUserInputAsText);
+
+
+
 
 
 // create an array to store to-do's
 
 var userDueDatesStoredArray = [];
+
+// function dueDateStore(input) {
+//     var dueDateText = document.createTextNode(input)
+//     return function () {
+//         userDueDatesStoredArray.push(dueDateText);
+//     }
+// }
+
+// submitButton.addEventListener("click", dueDateStore(userDueDateInput.input));
 
 function toDoName(name) {
     return this.name = name;
@@ -114,7 +170,7 @@ function toDoName(name) {
 
 
 
-submitButton.addEventListener("click", addUserInputToArray);
+// submitButton.addEventListener("click", addUserInputToArray);
 
 
 // storeUserInputIntoVar = function (input) {

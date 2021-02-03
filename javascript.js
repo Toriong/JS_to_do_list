a1f056f8039c74a12a2f90227882b480ead57e27
Toriong <67920345+Toriong@users.noreply.github.com>
2021-02-02 20:29:57 -0600

    combined, created, and deleted functions
    combined the following functions: 
    -clearAllToDos and clearAllComps functions
    
    deleted the following functions:
     -moveTaskToCompListArray()
     -delItemFromToDoArrayWhenCompButtonIsClicked()
    -saveCompList()
    -saveToDoList()
    -addToDoArray()
    
    
    Created putArrayItemsOntoDom() function which will bring all of the items from the arrays back onto the DOM. Also, created deleteDomLiItems() function which will delete all of the items on the DOM.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,12 @@
 
+
+var toDosAndCompletions = {
+    toDoArray: retrieveLocalStorageValue("Saved To Dos"),
+    compArray: retrieveLocalStorageValue("Saved completions")
+}
+
+
+
 var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
 script.type = 'text/javascript';
@@ -10,74 +18,112 @@ var userInput = document.querySelector(".to-do-input");
 
 var ToDoEntry = document.createTextNode(userInput.value)
 var submitButton = document.querySelector(".submit");
-submitButton.addEventListener("click", addToDo);
 
 
 
-var toDoArray = retrieveLocalStorageValue("Saved To Dos");
-var compList = retrieveLocalStorageValue("Saved completions");
 
-function moveTaskToCompListArray(task) {
-    compList.push(task);
-    saveCompList(compList);
-    console.log("Your completions: ", compList);
-    
+
+
+function deleteDomLiItems() {
+    toDoList.innerHTML = " ";
+    completionListOnDom.innerHTML = " ";
+    console.log("deleteDomLiItems was executed")
 }
+
+
+function putArrayItemsOntoDom() {
+    toDosAndCompletions.toDoArray.forEach(function (item) {
+        toDoList.prepend(liAndButtonsDom(item));
+    });
+    toDosAndCompletions.compArray.forEach(function (item) {
+        completionListOnDom.prepend(liAndButtonsDom(item));
+    });
+
+}
+
+function arrayCheck(userTask) {
+    if (toDosAndCompletions.toDoArray.includes(userTask)) {
+        alert("This item is already in your to do list.")
+    } else if (toDosAndCompletions.compArray.includes(userTask) && confirm("This item is in your completion list. Do you want to move it to your to do list? (Press 'OK' to confirm.)")) {
+        console.log(userTask);
+        var deleteMe = deleteSelectedItem(toDosAndCompletions.compArray, userTask);
+        deleteMe();
+        toDosAndCompletions.toDoArray.push(userTask);
+        save();
+        deleteDomLiItems();
+        putArrayItemsOntoDom();
+    } else (
+        
+        alert("No changes has occured.")
+    
+    )
+}
+
+
 function addToDo() {
-    var task = userInput.value;
-    if (toDoArray.length === null || 0) {
-        addToDoArray(task);
-    } else if (toDoArray.length !== null || 0) {
-        addToDoArray(task)
-    }
-    saveToDoList(toDoArray);
-    var newLi = liAndButtonsDom(task);
-    toDoList.prepend(newLi);
-    
-   
-}
-
-function deleteItemInArrayAndFromDom(deleteThisItem, li) {
     return function () {
-        if (confirm("This item will be deleted. Press 'OK' to continue.")) {
-            if (toDoArray.includes(deleteThisItem)) {
-                var index = toDoArray.indexOf(deleteThisItem);
-                toDoArray.splice(index, 1);
-                saveToDoList(toDoArray);
-            } else if (compList.includes(deleteThisItem)) {
-                var index = compList.indexOf(deleteThisItem);
-                compList.splice(index, 1);
-                saveCompList();
-            }
-            li.remove('li');
-            console.log("I was removed.")
-            alert("Item has been deleted.")
-        } else {
-            alert("No changes has occured.")
+        var userTask = userInput.value
+        if (toDosAndCompletions.toDoArray.includes(userTask) || toDosAndCompletions.compArray.includes(userTask)) {
+            arrayCheck(userTask);
+            return;
         }
+        toDosAndCompletions.toDoArray.push(userTask)
+        save();
+        deleteDomLiItems();
+        putArrayItemsOntoDom();
+        
+    }
+}
+
+
+submitButton.addEventListener("click", addToDo());
+
+
+function save() {
+    localStorage.setItem("Saved To Dos", JSON.stringify(toDosAndCompletions.toDoArray))
+    localStorage.setItem("Saved completions", JSON.stringify(toDosAndCompletions.compArray))
+}
+function deleteSelectedItem(array, inputFromUser) {
+    return function () {
+    var index = array.indexOf(inputFromUser);
+    array.splice(index, 1);
+    save();
+    deleteDomLiItems();
+    putArrayItemsOntoDom();
     }
     
 }
+        
 
+function completedButtonPromptAndMoveToComp(inputFromUser) {
+    return function () {
+        var deletion = deleteSelectedItem(toDosAndCompletions.toDoArray, inputFromUser);
+        toDosAndCompletions.compArray.push(inputFromUser);
+        deletion();
+        save();
+        deleteDomLiItems();
+        putArrayItemsOntoDom();
+    }
+}
 
 function liAndButtonsDom(userInput) {
     var liForList;
     var delButton = document.createElement("button");
     delButton.appendChild(document.createTextNode('DEL'));
     var newLi = document.createElement("li");
-    if (toDoArray.includes(userInput)) {
+    if (toDosAndCompletions.toDoArray.includes(userInput)) {
         newLi.appendChild(document.createTextNode(userInput));
         var completedButton = document.createElement("button");
         completedButton.appendChild(document.createTextNode("COMPELETED"));
-        newLi.appendChild(completedButton)    
+        newLi.appendChild(completedButton);    
         newLi.appendChild(delButton);
-        delButton.addEventListener("click", deleteItemInArrayAndFromDom(userInput, newLi));
-        completedButton.addEventListener("click", completedButtonPromptAndMoveToComp(newLi, completedButton, userInput))
+        delButton.addEventListener("click", deleteSelectedItem(toDosAndCompletions.toDoArray, userInput));
+        completedButton.addEventListener("click", completedButtonPromptAndMoveToComp(userInput))
         liForList = newLi;
-    } else if (compList.includes(userInput)) {
+    } else if (toDosAndCompletions.compArray.includes(userInput)) {
         newLi.appendChild(document.createTextNode(userInput));
         newLi.appendChild(delButton);
-        delButton.addEventListener("click", deleteItemInArrayAndFromDom(userInput, newLi));
+        delButton.addEventListener("click", deleteSelectedItem(toDosAndCompletions.compArray, userInput));
         liForList = newLi;
     }
     return liForList;
@@ -85,38 +131,6 @@ function liAndButtonsDom(userInput) {
 
 
 
-function delItemFromToDoArrayWhenCompButtonIsClicked(deleteThisItem) {
-    var index = toDoArray.indexOf(deleteThisItem);
-    toDoArray.splice(index, 1);
-    saveToDoList(toDoArray);
-    saveCompList();
-}
-
-function completedButtonPromptAndMoveToComp(listItem, compButton, task) {
-    return function () {
-        if (
-            confirm("You pressed 'completed.' This is task will now be inserted into your completion list Press 'Ok' to continue.")
-        ) {
-            moveTaskToCompListArray(task);
-            delItemFromToDoArrayWhenCompButtonIsClicked(task);
-            listItem.removeChild(compButton)
-            completionListOnDom.prepend(listItem);
-        }
-        else {
-            alert("No changes has occured.")
-        }
-    }
-}
-
-function saveToDoList(toDoArray) {
-    localStorage.setItem("Saved To Dos", JSON.stringify(toDoArray));
-}
-
-function addToDoArray(item) {
-    toDoArray.push(item);
-    saveToDoList(toDoArray);
-    console.log("to do array: ",toDoArray)
-}
 
 
 
@@ -127,33 +141,30 @@ var clearAll = document.getElementById("clear-all");
 var clearToDos = document.getElementById("clear-to-dos");
 var clearCompList = document.getElementById("clear-completions");
 var clearConfirmedButton = document.querySelector("#clear-button");
+var completionListOnDom = document.querySelector('#user-completion-list')
 
 
-function clearAllToDos() {
-    toDoArray.splice(0);
-    saveToDoList(toDoArray);
-    toDoList.innerHTML = " ";
-}
-function clearAllComps() {
-    compList.splice(0)
-    saveCompList();
-    completionListOnDom.innerHTML = " ";
+function clearSelectedArray(array) {
+    array.splice(0);
+    save();
+    deleteDomLiItems();
+    putArrayItemsOntoDom();
 }
 
 
 function clearDomList() {
     return function () {
-        if ((compList.length >= 0 || toDoArray.length >= 0) && clearOptions.value == "make a selection") {
+        if ((toDosAndCompletions.compArray.length >= 0 || toDosAndCompletions.toDoArray.length >= 0) && clearOptions.value == "make a selection") {
             alert("Please make a selection.")
         }
-        else if (toDoArray.length >= 1 && clearOptions.value == "CLEAR TO DOS" && confirm("Your to dos will now be deleted. Press 'OK' to continue.")) {
-            clearAllToDos();
-        } else if (compList.length >= 1 && clearOptions.value == "CLEAR COMPLETIONS" && confirm("Your completions will now be deleted. Press 'OK' to continue.")) {
-            clearAllComps();
-        } else if ((compList.length >=1 || toDoArray.length >=1)  && clearOptions.value == "CLEAR ALL" && confirm("Both your to dos and completions will now be deleted. Press 'OK' to continue.")) {
-            clearAllToDos();
-            clearAllComps();
-        } else if (compList.length===0 && toDoArray.length===0) {
+        else if (toDosAndCompletions.toDoArray.length >= 1 && clearOptions.value == "CLEAR TO DOS" && confirm("Your to dos will now be deleted. Press 'OK' to continue.")) {
+            clearSelectedArray(toDosAndCompletions.toDoArray);
+        } else if (toDosAndCompletions.compArray.length >= 1 && clearOptions.value == "CLEAR COMPLETIONS" && confirm("Your completions will now be deleted. Press 'OK' to continue.")) {
+            clearSelectedArray(toDosAndCompletions.compArray)
+        } else if ((toDosAndCompletions.compArray.length >=1 || toDosAndCompletions.toDoArray.length >=1)  && clearOptions.value == "CLEAR ALL" && confirm("Both your to dos and completions will now be deleted. Press 'OK' to continue.")) {
+            clearSelectedArray(toDosAndCompletions.toDoArray);
+            clearSelectedArray(toDosAndCompletions.compArray);
+        } else if (toDosAndCompletions.toDoArray.length===0 && toDosAndCompletions.compArray.length===0) {
             alert("There are no items in your to dos and completion list.")
         }
         else {
@@ -161,35 +172,27 @@ function clearDomList() {
         }
     }
 }
+
+
 clearConfirmedButton.addEventListener("click", clearDomList())
 
 
-function saveCompList() {
-    return localStorage.setItem("Saved completions", JSON.stringify(compList));
-}
 
 
-var completionListOnDom = document.querySelector('#user-completion-list')
+
 
 
 function retrieveLocalStorageValue(keyName) {
     var array = JSON.parse(localStorage.getItem(keyName))
-    console.log(array);
-    if (array == null) {
+    if (array == null || 0) {
         array = [];
     } else if (array.length >= 1) {}
     return array;
 }
 
-function putArrayOntoDom(array, domList) {
-    array.forEach(function (item) {
-        domList.prepend(liAndButtonsDom(item));
-    })
-    
-}
 
 
-putArrayOntoDom(toDoArray, toDoList);
-putArrayOntoDom(compList, completionListOnDom);
+
+putArrayItemsOntoDom();
 
 
